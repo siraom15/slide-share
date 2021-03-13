31924cb00375fe9df410dd9d69548a14151966d2
Siriwat <siriwatjaiyungyuen@gmail.com>
2021-03-14 00:06:45 +0700

    [ Add ] (Client) ✅ Sidebar Component

---
+++ client/components/sidebar.js
@@ -0,0 +1,66 @@
+import { Layout, Menu } from 'antd';
+import {
+    CloudUploadOutlined,
+    GlobalOutlined,
+    LockOutlined,
+    TagOutlined,
+    PushpinOutlined
+} from '@ant-design/icons';
+import Link from 'next/link';
+import { useState } from 'react';
+const { SubMenu } = Menu;
+const { Sider } = Layout;
+const SideBar = ({ isLoggedIn = true, page = '1' }) => {
+    return (
+        <Sider collapsible={true} width={200}
+            style={{
+                backgroundColor: "white"
+            }}
+        >
+
+            <Menu
+                mode="inline"
+                defaultSelectedKeys={page}
+                defaultOpenKeys={['sub1']}
+            >
+
+                <Menu.Item key="1" icon={<GlobalOutlined />}>
+                    <Link href="/">
+                        <a>
+                            Public Slide
+                        </a>
+                    </Link>
+                </Menu.Item>
+                {
+                    isLoggedIn ?
+                        <>
+                            <Menu.Item key="2" icon={<CloudUploadOutlined />}>
+                                <Link href="/upload">
+                                    <a>
+                                        Upload Slide
+                                    </a>
+                                </Link>
+                            </Menu.Item>
+                            <Menu.Item key="3" icon={<PushpinOutlined />}>
+                                <Link href="/">
+                                    <a>
+                                        Saved Slide
+                                    </a>
+                                </Link>
+                            </Menu.Item>
+                            <Menu.Item key="4" icon={<LockOutlined />}>
+                                <Link href="/myslide">
+                                    <a>
+                                        My Slide
+                                    </a>
+                                </Link>
+                            </Menu.Item>
+                        </>
+                        :
+                        null
+                }
+            </Menu>
+        </Sider>
+    )
+}
+export default SideBar;
