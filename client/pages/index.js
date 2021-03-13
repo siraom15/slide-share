eb5f52865b456dad08bfa02d25e0188f8eeff7fc
Siriwat <siriwatjaiyungyuen@gmail.com>
2021-03-13 23:59:05 +0700

    [ Add ] (Client) ✅ Index Page

---
+++ client/pages/index.js
@@ -0,0 +1,32 @@
+// import Link from 'next/link';
+import Head from 'next/head'
+import { Layout, BackTop } from 'antd';
+import SideBar from '../components/sidebar'
+import Navbar from '../components/navbar';
+import AllPublic from '../components/allPublic';
+const { Content, Footer } = Layout;
+
+
+const Home = () => {
+  return (
+    <>
+      <Head>
+        <title>Home | Slide Share</title>
+      </Head>
+      <Layout >
+        <Navbar isLoggedIn={true} />
+        <Content>
+          <Layout >
+            <SideBar page="1" />
+            <Content style={{ backgroundColor: 'white' }}>
+              <AllPublic />
+            </Content>
+          </Layout>
+        </Content>
+        <BackTop />
+      </Layout>
+    </>
+  )
+}
+
+export default Home
