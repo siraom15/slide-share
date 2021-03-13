6f9192ff5e895411eff5bb1460a91b425aed6621
Siriwat <siriwatjaiyungyuen@gmail.com>
2021-03-13 23:45:30 +0700

    [ Add ] ✅ Add Database Config

---
+++ server/db/db.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    mongouri : "mongodb+srv://share-slide:F25RA1WB6i44G1WU@gocourse.wzkcz.mongodb.net/share-slide?retryWrites=true&w=majority",
+    username : "share-slide",
+    password : "F25RA1WB6i44G1WU"
+}
