2757cb1fc15531cde3f08c0788fea120463ca8ba
Siriwat <siriwatjaiyungyuen@gmail.com>
2021-03-14 00:07:00 +0700

    [ Add ] (Client) ✅ Footer Component

---
+++ client/components/footer.js
@@ -0,0 +1,8 @@
+const Footer = () => {
+    return (
+        <Footer style={{ textAlign: 'center', backgroundColor: "white" }}>
+            ©Slide Share {new Date().getFullYear()}
+        </Footer>
+    )
+}
+export default Footer;
